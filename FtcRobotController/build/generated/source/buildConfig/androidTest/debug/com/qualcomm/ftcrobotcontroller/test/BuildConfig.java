9ee908c4e140e3d3ba6a2a0d0f24a60a1143e1af
Cr-N <98693811+Cr-N@users.noreply.github.com>
2024-04-28 21:32:03 +0300

    We live we love we...

---
+++ FtcRobotController/build/generated/source/buildConfig/androidTest/debug/com/qualcomm/ftcrobotcontroller/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.qualcomm.ftcrobotcontroller.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String APP_BUILD_TIME = "2024-04-28T21:28:04.718+0300";
+}

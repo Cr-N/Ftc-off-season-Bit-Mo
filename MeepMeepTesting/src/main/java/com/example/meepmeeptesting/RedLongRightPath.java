270524248cdd0f7f7acadc2f1cac04379a52ddb5
Cr-N <98693811+Cr-N@users.noreply.github.com>
2024-06-22 15:45:50 +0300

    Red Long Right 2 + 5!

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedLongRightPath.java
@@ -0,0 +1,109 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+/// 2+5 yuppi :)
+public class RedLongRightPath {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-35.2, -63.2, Math.PI/2))
+
+
+                //Spike mark RIGHT
+                .lineToYSplineHeading(-43,Math.PI/3+0.25)
+                .splineToLinearHeading(new Pose2d(-31,-35,Math.PI/4),0)
+
+                .waitSeconds(0.5)
+                //Go back a little
+                .strafeToSplineHeading(new Vector2d(-46.5,-38),Math.PI/2+Math.PI/6)
+                //Go to stack
+                .splineToLinearHeading(new Pose2d(-56,-36.5,Math.PI),2)
+
+                // Intake one pixel
+                .waitSeconds(1)
+
+                //Go to leaving spot
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(-35.4,-58.9),0)
+
+                // Go forward
+                .strafeToConstantHeading(new Vector2d(0,-58.9))
+
+                //Spline to Backboard on the RIGHT side(closer to us so It's faster)
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(48.4,-40.4,-1e-6),0.50)
+
+                // Deploy Yellow + 1st White
+                .waitSeconds(1)
+
+                //Align to get back to the stack(on the bottom)
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(13,-58.9,-Math.PI),Math.PI)  // 4.3 // tangent: Math.PI works reeeealy well
+
+                //Spline to Stack for intake
+                .setReversed(false)
+                .splineToConstantHeading(new Vector2d(-56,-36.5),1.75)
+
+                // Intake 2nd and 3rd Pixels
+                .waitSeconds(1)
+
+                //Go to leaving spot
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(-35.4,-58.9),0)
+
+                // Go forward
+                .strafeToConstantHeading(new Vector2d(0,-58.9))
+
+                //Spline to Backboard on the RIGHT side(closer to us so It's faster)
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(48.4,-40.4,0),0.50)
+
+                //Deploy 2nd and 3rd Pixels
+                .waitSeconds(1)
+
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(13,-58.9,-Math.PI),Math.PI)  // 4.3 // tangent: Math.PI works reeeealy well
+
+                //Spline to Stack for intake
+                .setReversed(false)
+                .splineToConstantHeading(new Vector2d(-56,-36.5),1.75)
+
+                // Intake 2nd and 3rd Pixels
+                .waitSeconds(1)
+
+                //Go to leaving spot
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(-35.4,-58.9),0)
+
+                // Go forward
+                .strafeToConstantHeading(new Vector2d(0,-58.9))
+
+                //Spline to Backboard on the RIGHT side(closer to us so It's faster)
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(48.4,-40.4,0),0.50)
+
+                //Deploy 2nd and 3rd Pixels
+                .waitSeconds(1)
+                //Park in the corner
+                .strafeTo(new Vector2d(48.3,-48))
+
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}

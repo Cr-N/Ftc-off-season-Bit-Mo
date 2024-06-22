c3b33bb9aa5df1744d49647cb0ee3c5f9f84fd29
Cr-N <98693811+Cr-N@users.noreply.github.com>
2024-06-22 13:38:16 +0300

    Red Long Middle 2+5!

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,72 +17,9 @@ public class MeepMeepTesting {
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(11.5, -63.2, Math.PI/2))
 
-                // SPIKE MARK **LEFT**
-                //.lineToY(-40.2)
-                //.splineToLinearHeading(new Pose2d(15,-36.2, 0.5235987755982988),0) // Math.PI/6
-                                .strafeToConstantHeading(new Vector2d(22,-36.2))
-                .strafeToConstantHeading(new Vector2d(22,-39.2))
-                // Mergi putin in spate ca sa nu dai in pixelul MOV
-                //.strafeTo(new Vector2d(22,-50))  // 14 , -40
-                .strafeTo(new Vector2d(30,-45))  // 14 , -40
-                //.splineToLinearHeading(new Pose2d(24,-50,0),0)
+                        .strafeToLinearHeading(new Vector2d(11.5,-40.2),0)
+                        .strafeToSplineHeading(new Vector2d(45,-40.2),0)
 
-                // Spline catre Backdrop 1
-                .splineToLinearHeading(new Pose2d(48.4,-30.4,0),0)
-                // deploy pixel 1
-                .waitSeconds(1)
-
-                //  mergi in spate putin
-                .lineToX(45)
-                .lineToXSplineHeading(42.4,Math.PI/2)
-
-                // Mergi la Pozitia de unde PLECI CATRE STACK
-                .strafeToLinearHeading(new Vector2d(44.5 , -12),Math.PI)
-
-                // Mergi la STACK x = -56 y = -12 !!!!! VERY IMPORTANT
-                .strafeToConstantHeading(new Vector2d(-56 , -12))
-
-                // intake pixel 1
-                .waitSeconds(1)
-
-                // mergi inapoi la pozitia de unde ai plecat ca sa pleci catre STACK
-                .lineToX(-50) // -43
-                .lineToXLinearHeading(-43,0)
-                .lineToXConstantHeading(44.5)
-
-
-                // Spline catre Backdrop 2
-                .splineToConstantHeading(new Vector2d(48.4, -35.4), 0)
-
-                // deploy pixel 2
-                .waitSeconds(1)
-
-                //  mergi in spate putin
-                .lineToX(45)
-                .lineToXSplineHeading(42.4,Math.PI/2)
-
-                // Mergi la Pozitia de unde PLECI CATRE STACK
-                .strafeToLinearHeading(new Vector2d(44.5 , -12),Math.PI)
-
-                // Mergi la STACK x = -56 y = -12 !!!!! VERY IMPORTANT
-                .strafeToConstantHeading(new Vector2d(-56 , -12))
-
-                // intake pixel 2
-                .waitSeconds(1)
-
-                // mergi inapoi la pozitia de unde ai plecat ca sa pleci catre STACK
-                .lineToX(-50) // -43
-                .lineToXLinearHeading(-43,0)
-                .lineToXConstantHeading(44.5)
-
-                // Spline catre Backdrop 2
-                .splineToConstantHeading(new Vector2d(48.4, -35.4), 0)
-
-                // deploy pixel 3
-                .waitSeconds(1)
-
-                // Mergi la STACK x = -56 y = -12 !!!!! VERY IMPORTANT
-                .strafeToConstantHeading(new Vector2d(47.3 , -12))
 
                 .build());
 
--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedLongMiddlePath.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedLongMiddlePath.java
@@ -16,38 +16,57 @@ public class RedLongMiddlePath {
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-35.2, -63.2, Math.PI/2))
 
-
                 //Spike mark MIDDLE
                 .strafeToConstantHeading(new Vector2d(-36,-33.5))
 
                 .waitSeconds(0.3)
+
                 //Go back a little
                 .strafeToSplineHeading(new Vector2d(-46.5,-38),Math.PI/2+Math.PI/6)
+
                 //Go to stack
                 .splineToLinearHeading(new Pose2d(-56,-35.7,Math.PI),2) //-36.5
 
                 // Intake one pixel
                 .waitSeconds(1)
 
-                // Spline to Backboard on the MIDDLE side
+                // Spline to Backboard on the MIDDLE side(1st to travel the majority of distance, 2nd to turn and position)
                 .setReversed(true)
                 .splineToConstantHeading(new Vector2d(20,-35.4),0)
+                .splineToLinearHeading(new Pose2d(48.4,-35.4,0),0)
 
-                                .splineToLinearHeading(new Pose2d(48.4,-35.4,0),0)
+                //Go to stack(1st to travel the majority of distance, 2nd to turn and position)
+                .strafeToConstantHeading(new Vector2d(-32.5,-35.7)) // -32.5
+                .splineToSplineHeading(new Pose2d(-56,-35.7,-Math.PI),Math.PI/2+0.7)  // Math.PI/2+0.7
 
-                //Go to stack
-               // .splineToLinearHeading(new Pose2d(-56,-35.7,Math.PI),2)
-                                .strafeToConstantHeading(new Vector2d(-32.5,-35.7)) //-56
+                //Intake pixles nr 2 and 3
+                .waitSeconds(1)
 
-                        //.strafeToLinearHeading(new Vector2d(-56,-35.7),Math.PI
-                        .setReversed(true)
-                        
-                        .strafeToSplineHeading(new Vector2d(-56,-35.7),Math.PI)
-                //Park in the corner
-                //.strafeTo(new Vector2d(48.3,-58))
+                // Spline to Backboard on the MIDDLE side(1st to travel the majority of distance, 2nd to turn and position)
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(20,-35.4),0)
+                .splineToLinearHeading(new Pose2d(48.4,-35.4,0),0)
 
+                // Deploy 2nd and 3rd pixels
+                .waitSeconds(1)
 
+                //Go to stack(1st to travel the majority of distance, 2nd to turn and position)
+                .strafeToConstantHeading(new Vector2d(-32.5,-35.7)) // -32.5
+                .splineToSplineHeading(new Pose2d(-56,-35.7,-Math.PI),Math.PI/2+0.7)
 
+                //Intake 4th and 5th pixels
+                .waitSeconds(1)
+
+                // Spline to Backboard on the MIDDLE side(1st to travel the majority of distance, 2nd to turn and position)
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(20,-35.4),0)
+                .splineToLinearHeading(new Pose2d(48.4,-35.4,0),0)
+
+                // Deploy 4th and 5th pixels
+                .waitSeconds(1)
+
+                // Park in the corner
+                .strafeTo(new Vector2d(48.3,-58))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)

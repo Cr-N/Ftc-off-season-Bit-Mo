cd2aaa586c08a73ef2660aca4b16beefeee1a0d0
Cr-N <cristeanicolaeandrei@gmail.com>
2024-07-14 13:51:23 +0300

    Testing commit on my pc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/Arm_Rotate_Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/Arm_Rotate_Testing.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
+// sk
 @Config
 @TeleOp
 public class Arm_Rotate_Testing extends LinearOpMode {
